340efc4a33a21dd28bafb52f424593f289c4d022
Mate Kukri <km@mkukri.xyz>
2022-12-05 19:46:07 +0000

    Reduce code duplication in the compiler

--- src/compile.rs
+++ src/compile.rs
@@ -86,7 +86,7 @@ impl<'a> Compiler<'a> {
 
   /// Compile an expression for its value
 
-  fn compile_value_expr(&mut self, expr: &ast::Expr) -> Result<(), CErr> {
+  fn compile_expr(&mut self, expr: &ast::Expr) -> Result<(), CErr> {
     match expr {
       ast::Expr::Nil => { self.vm.emit_const(Val::Nil); }
       ast::Expr::Bool(b) => { self.vm.emit_const(Val::Bool(b.clone())); }
@@ -95,191 +95,115 @@ impl<'a> Compiler<'a> {
       ast::Expr::Id(id) => { self.vm.emit(Insn::Load(id.clone())); }
       ast::Expr::Call(func, args) => {
         for arg in args.iter() {
-          self.compile_value_expr(arg)?;
+          self.compile_expr(arg)?;
         }
-        self.compile_value_expr(func)?;
+        self.compile_expr(func)?;
         self.vm.emit(Insn::Call(args.len()));
       }
       ast::Expr::Dot(obj, id) => {
-        self.compile_value_expr(obj)?;
+        self.compile_expr(obj)?;
         self.vm.emit(Insn::LoadField(id.clone()));
       }
       ast::Expr::Un(op, arg) => {
-        self.compile_value_expr(arg)?;
+        self.compile_expr(arg)?;
         self.vm.emit(Insn::Un(op.clone()));
       }
       ast::Expr::Bin(op, lhs, rhs) => {
-        self.compile_value_expr(lhs)?;
-        self.compile_value_expr(rhs)?;
+        self.compile_expr(lhs)?;
+        self.compile_expr(rhs)?;
         self.vm.emit(Insn::Bin(op.clone()));
       }
       ast::Expr::And(lhs, rhs) => {
-        self.compile_value_expr(lhs)?;
+        self.compile_expr(lhs)?;
         self.vm.emit(Insn::Dup);
         let patch1 = self.vm.emit(Insn::JumpFalse(0));
         self.vm.emit(Insn::Discard);
-        self.compile_value_expr(rhs)?;
+        self.compile_expr(rhs)?;
         let end_ip = self.vm.insn_cnt();
         self.vm.patch(patch1, Insn::JumpFalse(end_ip));
       }
       ast::Expr::Or(lhs, rhs) => {
-        self.compile_value_expr(lhs)?;
+        self.compile_expr(lhs)?;
         self.vm.emit(Insn::Dup);
         let patch1 = self.vm.emit(Insn::JumpTrue(0));
         self.vm.emit(Insn::Discard);
-        self.compile_value_expr(rhs)?;
+        self.compile_expr(rhs)?;
         let end_ip = self.vm.insn_cnt();
         self.vm.patch(patch1, Insn::JumpTrue(end_ip));
       }
       ast::Expr::Block(exprs) => {
         if exprs.len() > 0 {
-          self.compile_enter();
-          {
+          self.compile_scoped(|this| {
             // Evaluate the first N-1 expressions for side effects
             for expr in exprs[0..exprs.len()-1].iter() {
-              self.compile_side_effect_expr(expr)?;
+              this.compile_expr(expr)?;
+              this.vm.emit(Insn::Discard);
             }
             // Evaluate the last expression for its value
-            self.compile_value_expr(&exprs[exprs.len()-1])?;
-          }
-          self.compile_exit();
+            this.compile_expr(&exprs[exprs.len()-1])?;
+            Ok(())
+          })?;
         } else {
           // If there are no expressions the block yields nil
           self.vm.emit_const(Val::Nil);
         }
       }
+      ast::Expr::Var(id, val) => {
+        self.compile_expr(val)?;
+        self.vm.emit(Insn::Declare(id.clone()));
+
+        self.vm.emit_const(Val::Nil);
+      }
+      ast::Expr::As(dest, val) => {
+        self.compile_expr(val)?;
+        self.compile_assignment(dest)?;
+
+        self.vm.emit_const(Val::Nil);
+      }
+      ast::Expr::Continue => {
+        self.compile_continue()?;
+
+        self.vm.emit_const(Val::Nil);
+      }
+      ast::Expr::Break => {
+        self.compile_break()?;
+
+        self.vm.emit_const(Val::Nil);
+      }
+      ast::Expr::Return(val) => {
+        self.compile_expr(val)?;
+        self.compile_return()?;
+
+        self.vm.emit_const(Val::Nil);
+      }
       ast::Expr::If(cond, arg1, arg2) => {
         // Compile conditional jump
-        self.compile_value_expr(cond)?;
+        self.compile_expr(cond)?;
         let patch1 = self.vm.emit(Insn::JumpFalse(0));
         // Compile true case
-        self.compile_value_expr(arg1)?;
+        self.compile_expr(arg1)?;
         let patch2 = self.vm.emit(Insn::Jump(0));
         // Compile false case
         let false_ip = self.vm.insn_cnt();
-        self.compile_value_expr(arg2)?;
+        self.compile_expr(arg2)?;
         // Backpatch jump targets
         self.vm.patch(patch1, Insn::JumpFalse(false_ip));
         let end_ip = self.vm.insn_cnt();
         self.vm.patch(patch2, Insn::Jump(end_ip));
-      }
-      ast::Expr::Lambda(params, body) => {
-        let func_ip = self.compile_func(body)?;
-        self.vm.emit(Insn::Func(params.clone(), func_ip));
-      }
-      // These expressions only have side effects, and always yield nil
-      ast::Expr::Var(..) |
-      ast::Expr::As(..) |
-      ast::Expr::Continue |
-      ast::Expr::Break |
-      ast::Expr::Return(..) |
-      ast::Expr::While(..) |
-      ast::Expr::For(..) |
-      ast::Expr::Func(..) |
-      ast::Expr::Type(..) => {
-        self.compile_side_effect_expr(expr)?;
-        self.vm.emit_const(Val::Nil);
-      }
-    }
-    Ok(())
-  }
-
-  /// Compile an expression for its side effects
-
-  fn compile_side_effect_expr(&mut self, expr: &ast::Expr) -> Result<(), CErr> {
-    match expr {
-      ast::Expr::Nil |
-      ast::Expr::Bool(..) |
-      ast::Expr::Int(..) |
-      ast::Expr::Str(..) |
-      ast::Expr::Id(..) |
-      ast::Expr::Lambda(..) => {}
-      ast::Expr::Call(func, args) => {
-        for arg in args.iter() {
-          self.compile_value_expr(arg)?;
-        }
-        self.compile_value_expr(func)?;
-        self.vm.emit(Insn::Call(args.len()));
-        self.vm.emit(Insn::Discard);
-      }
-      ast::Expr::Dot(obj, _) => {
-        self.compile_side_effect_expr(obj)?;
-      }
-      ast::Expr::Un(_, arg) => {
-        self.compile_side_effect_expr(arg)?;
-      }
-      ast::Expr::Bin(_, lhs, rhs) => {
-        self.compile_side_effect_expr(lhs)?;
-        self.compile_side_effect_expr(rhs)?;
-      }
-      ast::Expr::And(lhs, rhs) => {
-        self.compile_value_expr(lhs)?;
-        let patch1 = self.vm.emit(Insn::JumpFalse(0));
-        self.compile_side_effect_expr(rhs)?;
-        let end_ip = self.vm.insn_cnt();
-        self.vm.patch(patch1, Insn::JumpFalse(end_ip));
-      }
-      ast::Expr::Or(lhs, rhs) => {
-        self.compile_value_expr(lhs)?;
-        let patch1 = self.vm.emit(Insn::JumpTrue(0));
-        self.compile_side_effect_expr(rhs)?;
-        let end_ip = self.vm.insn_cnt();
-        self.vm.patch(patch1, Insn::JumpTrue(end_ip));
-      }
-      ast::Expr::Block(exprs) => {
-        if exprs.len() > 0 {
-          self.compile_enter();
-          {
-            for expr in exprs.iter() {
-              self.compile_side_effect_expr(expr)?;
-            }
-          }
-          self.compile_exit();
-        }
-      }
-      ast::Expr::Var(id, val) => {
-        self.compile_value_expr(val)?;
-        self.vm.emit(Insn::Declare(id.clone()));
-      }
-      ast::Expr::As(dest, val) => {
-        self.compile_value_expr(val)?;
-        self.compile_assignment(dest)?;
-      }
-      ast::Expr::Continue => {
-        self.compile_continue()?;
-      }
-      ast::Expr::Break => {
-        self.compile_break()?;
-      }
-      ast::Expr::Return(val) => {
-        self.compile_value_expr(val)?;
-        self.compile_return()?;
-      }
-      ast::Expr::If(cond, arg1, arg2) => {
-        // Compile conditional jump
-        self.compile_value_expr(cond)?;
-        let patch1 = self.vm.emit(Insn::JumpFalse(0));
-        // Compile true case
-        self.compile_side_effect_expr(arg1)?;
-        let patch2 = self.vm.emit(Insn::Jump(0));
-        // Compile false case
-        let false_ip = self.vm.insn_cnt();
-        self.compile_side_effect_expr(arg2)?;
-        // Backpatch jump targets
-        self.vm.patch(patch1, Insn::JumpFalse(false_ip));
-        self.vm.patch(patch2, Insn::Jump(self.vm.insn_cnt()));
       }
       ast::Expr::While(cond, body) => {
         // Save continue pooint
         let cont_ip = self.vm.insn_cnt();
 
         // Compile conditional
-        self.compile_value_expr(cond)?;
+        self.compile_expr(cond)?;
         let patch1 = self.vm.emit(Insn::JumpFalse(0));
 
         // Compile loop body
         let ctx = self.compile_loop(|this| {
-          this.compile_side_effect_expr(body)
+          this.compile_expr(body)?;
+          this.vm.emit(Insn::Discard);
+          Ok(())
         })?;
         // Jump back to start
         self.vm.emit(Insn::Jump(cont_ip));
@@ -295,10 +219,12 @@ impl<'a> Compiler<'a> {
         for patch in ctx.breaks.iter() {
           self.vm.patch(*patch, Insn::Jump(brk_ip));
         }
+
+        self.vm.emit_const(Val::Nil);
       }
       ast::Expr::For(id, iter, body) => {
         // Push iterator to stack
-        self.compile_value_expr(iter)?;
+        self.compile_expr(iter)?;
 
         // Save continue point
         let cont_ip = self.vm.insn_cnt();
@@ -316,11 +242,12 @@ impl<'a> Compiler<'a> {
 
         // Compile loop body
         let ctx = self.compile_loop(|this| {
-          this.compile_enter();
-          this.vm.emit(Insn::Declare(id.clone()));
-          this.compile_side_effect_expr(body)?;
-          this.compile_exit();
-          Ok(())
+          this.compile_scoped(|this| {
+            this.vm.emit(Insn::Declare(id.clone()));
+            this.compile_expr(body)?;
+            this.vm.emit(Insn::Discard);
+            Ok(())
+          })
         })?;
 
         // Jump back to start
@@ -341,11 +268,15 @@ impl<'a> Compiler<'a> {
         for patch in ctx.breaks.iter() {
           self.vm.patch(*patch, Insn::Jump(brk_ip));
         }
+
+        self.vm.emit_const(Val::Nil);
       }
       ast::Expr::Func((id, params, body)) => {
         let func_ip = self.compile_func(body)?;
         self.vm.emit(Insn::Func(params.clone(), func_ip));
         self.vm.emit(Insn::Declare(id.clone()));
+
+        self.vm.emit_const(Val::Nil);
       }
       ast::Expr::Type(id, fields, parsed_methods) => {
         let mut methods = vec![];
@@ -355,9 +286,28 @@ impl<'a> Compiler<'a> {
         }
         self.vm.emit(Insn::Ctor(fields.clone(), methods));
         self.vm.emit(Insn::Declare(id.clone()));
+
+        self.vm.emit_const(Val::Nil);
+      }
+      ast::Expr::Lambda(params, body) => {
+        let func_ip = self.compile_func(body)?;
+        self.vm.emit(Insn::Func(params.clone(), func_ip));
       }
     }
     Ok(())
+  }
+
+  fn compile_scoped<F>(&mut self, cb: F) -> Result<(), CErr>
+    where F: Fn(&mut Compiler) -> Result<(), CErr>
+  {
+    if let Some(curfunc) = self.funcs.last_mut() { curfunc.block_cnt += 1; }
+    if let Some(curloop) = self.loops.last_mut() { curloop.block_cnt += 1; }
+    self.vm.emit(Insn::Enter);
+    let result = cb(self);
+    if let Some(curfunc) = self.funcs.last_mut() { curfunc.block_cnt -= 1; }
+    if let Some(curloop) = self.loops.last_mut() { curloop.block_cnt -= 1; }
+    self.vm.emit(Insn::Exit);
+    result
   }
 
   fn compile_loop<F>(&mut self, cb: F) -> Result<LoopCtx, CErr>
@@ -371,15 +321,14 @@ impl<'a> Compiler<'a> {
     Ok(self.loops.pop().unwrap())
   }
 
-  fn compile_func(&mut self, body: &ast::Expr) -> Result<usize, CErr>
-  {
+  fn compile_func(&mut self, body: &ast::Expr) -> Result<usize, CErr> {
     // Jump over function
     let patch1 = self.vm.emit(Insn::Jump(0));
     // Save beginning of the function
     let func_ip = self.vm.insn_cnt();
     // Compile function body
     self.funcs.push(FuncCtx { block_cnt: 0 });
-    self.compile_value_expr(body)?;
+    self.compile_expr(body)?;
     self.vm.emit(Insn::Return);
     self.funcs.pop();
     // Backpatch jump target
@@ -395,7 +344,7 @@ impl<'a> Compiler<'a> {
         Ok(())
       }
       ast::Expr::Dot(obj, id) => {
-        self.compile_value_expr(obj)?;
+        self.compile_expr(obj)?;
         self.vm.emit(Insn::StoreField(id.clone()));
         Ok(())
       }
@@ -403,26 +352,6 @@ impl<'a> Compiler<'a> {
         Err(CErr::LValueRequired)
       }
     }
-  }
-
-  fn compile_enter(&mut self) {
-    if let Some(curfunc) = self.funcs.last_mut() {
-      curfunc.block_cnt += 1;
-    }
-    if let Some(curloop) = self.loops.last_mut() {
-      curloop.block_cnt += 1;
-    }
-    self.vm.emit(Insn::Enter);
-  }
-
-  fn compile_exit(&mut self) {
-    if let Some(curfunc) = self.funcs.last_mut() {
-      curfunc.block_cnt -= 1;
-    }
-    if let Some(curloop) = self.loops.last_mut() {
-      curloop.block_cnt -= 1;
-    }
-    self.vm.emit(Insn::Exit);
   }
 
   fn compile_continue(&mut self) -> Result<(), CErr> {
@@ -461,7 +390,8 @@ impl<'a> Compiler<'a> {
 pub fn compile_program(vm: &mut Vm, ast::Program(exprs): &ast::Program) -> Result<(), CErr> {
   let mut compiler = Compiler::new(vm);
   for expr in exprs.iter() {
-    compiler.compile_side_effect_expr(expr)?;
+    compiler.compile_expr(expr)?;
+    compiler.vm.emit(Insn::Discard);
   }
   compiler.vm.emit(Insn::Terminate);
   Ok(())
